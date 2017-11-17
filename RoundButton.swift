fc2f396fe0e0a9778e1451d5a899b1f0bb1f536c
liuzhen <lazyjean@foxmail.com>
2017-11-17 17:05:12 +0800

    初版完成

---
+++ RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  EKit
+//
+//  Created by 刘真 on 2017/11/17.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ViewExtension.swift
+//  EKit
+//
+//  Created by 刘真 on 2017/11/17.
+//
+
+import Foundation
