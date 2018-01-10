bdd200e10ac5d8158f78212ef8fbfec190400554
刘真 <lazy66@me.com>
2018-01-11 07:30:21 +0800

    添加RoundLabel

---
+++ EKit/RoundLabel.swift
@@ -0,0 +1,25 @@
+//
+//  RoundLabel.swift
+//  EKit
+//
+//  Created by 刘真 on 11/01/2018.
+//
+
+import UIKit
+
+@IBDesignable
+
+class RoundLabel: UILabel {
+    @IBInspectable var radius: CGFloat = 5 {
+        didSet {
+            layer.cornerRadius = radius
+            clipsToBounds = true
+        }
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = radius
+        clipsToBounds = true
+    }
+}
