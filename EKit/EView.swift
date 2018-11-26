c0141ffbb52c1286c8960838bfd2fd7a7427e884
刘真 <lazy66@me.com>
2018-11-26 22:06:42 +0800

    EView添加设置过渡色

--- EKit/EView.swift
+++ EKit/EView.swift
@@ -33,15 +33,70 @@ open class EView: UIView {
         }
     }
 
+    //最大圆角
+    @IBInspectable var autoMaxCornerRadius: Bool = false {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    //过渡色
+    @IBInspectable var gradient: Bool = false {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var gradientColor1: UIColor = UIColor.white {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var gradientColor2: UIColor = UIColor.black {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var gradientAngle: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    lazy var gradientLayer: CAGradientLayer = { [unowned self] in
+        let layer = CAGradientLayer()
+        layer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+        layer.speed = 0
+        layer.timeOffset = 0
+        layer.transform = CATransform3DMakeRotation(gradientAngle/180*CGFloat.pi, 0, 0, 1)
+        self.layer.addSublayer(layer)
+        return layer
+    }()
+    
     open override func layoutSubviews() {
         super.layoutSubviews()
-        layer.cornerRadius = radius
+        
+        //当设置了自动最大圆角时，忽略普通的圆角设设置
+        if autoMaxCornerRadius {
+            let maxSide = min(layer.frame.width, layer.frame.height)
+            layer.cornerRadius = maxSide/2.0
+        } else {
+            layer.cornerRadius = radius
+        }
         
         clipsToBounds = layer.cornerRadius > 0
         
         //设置边框
         layer.borderColor = borderColor.cgColor
         layer.borderWidth = borderWidth
+        
+        if self.gradient {
+            self.gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+            self.gradientLayer.transform = CATransform3DMakeRotation(gradientAngle/180*CGFloat.pi, 0, 0, 1)
+            self.gradientLayer.frame = self.bounds
+        }
     }
     
     override open func prepareForInterfaceBuilder() {
